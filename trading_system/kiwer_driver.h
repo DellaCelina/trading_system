b69e88cadcee539ab385d641bc584a0660266112
test <test@test.com>
2026-07-03 14:47:35 +0900

    [feat] api private

--- trading_system/kiwer_driver.h
+++ trading_system/kiwer_driver.h
@@ -4,23 +4,24 @@
 #include "kiwer_api.h"
 
 struct KiwerDriver : public StockBrokerDriverInterface {
-    KiwerDriver(KiwerAPI* api) : api(api) {}
+    KiwerDriver(KiwerAPI* api) { kiwer_api = api;}
 
     void login(const std::string& id, const std::string& pass) override {
-        api->login(id, pass);
+        return kiwer_api->login(id, pass);
     }
 
     void buy(const std::string& stockCode, int price, int count) override {
-        api->buy(stockCode, count, price);
+        return kiwer_api->buy(stockCode, count, price);
     }
 
     void sell(const std::string& stockCode, int price, int count) override {
-        api->sell(stockCode, count, price);
+        return kiwer_api->sell(stockCode, count, price);
     }
 
     int getPrice(const std::string& stockCode) override {
-        return api->currentPrice(stockCode);
+        return kiwer_api->currentPrice(stockCode);
     }
 
-    KiwerAPI* api;
+private:
+    KiwerAPI* kiwer_api;
 };
