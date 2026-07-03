29a4868a8c3832f295e09ab44249e42923626fd1
minji-choe-0559 <mia827.oh@emcore.kr>
2026-07-03 13:43:23 +0900

    [feat] Implement AutoTradingSystem::buyNiceTiming
    - 200ms 주기로 3회 가격을 조회하여 상승 추세를 판단
    - 상승 추세일 경우 마지막 조회가로 총금액 한도 내 최대 수량을 매수
    - selectStockBroker/setTimer가 Driver/Timer를 DI

--- trading_system/auto_trading_system.h
+++ trading_system/auto_trading_system.h
@@ -8,16 +8,32 @@
 class AutoTradingSystem {
 public:
     //Dependency Injection for timer
-    void setTimer(TimerInterface* timer) {}
+    void setTimer(TimerInterface* timer) {
+        this->timer = timer;
+    }
 
     //증권사선택기능: selectStockBrocker( )
-    void selectStockBroker(StockBrokerDriverInterface* driver) {}
+    void selectStockBroker(StockBrokerDriverInterface* driver) {
+        this->driver = driver;
+    }
 
     //기능1 : buyNiceTiming(종목, 총 금액)
     //• 200ms 주기로 3회 가격을 읽고, 가격이올라가는추세인지파악한다.
     //• 가격이올라가는추세라면, 총금액을최대한사용하여최대수량만큼매수한다.
     //• 마지막에읽은가격으로매수한다.
-    void buyNiceTiming(const std::string& stockCode, int totalMoney) {}
+    void buyNiceTiming(const std::string& stockCode, int totalMoney) {
+        int firstPrice = driver->getPrice(stockCode);
+        timer->sleep(200);
+        int secondPrice = driver->getPrice(stockCode);
+        timer->sleep(200);
+        int thirdPrice = driver->getPrice(stockCode);
+
+        bool isRising = firstPrice < secondPrice && secondPrice < thirdPrice;
+        if (isRising) {
+            int count = totalMoney / thirdPrice;
+            driver->buy(stockCode, thirdPrice, count);
+        }
+    }
 
     //▪ 기능2 : sellNiceTiming(종목, 수량)
     //• 200ms 주기로 3회 가격을 읽고, 가격이 내려가는추세인지파악한다.
