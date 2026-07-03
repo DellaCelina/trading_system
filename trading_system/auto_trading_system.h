84cd755ce32ee3086b85ee11fc0fc49ed4cb7653
mgKang <kangtop729@gmail.com>
2026-07-03 12:45:31 +0900

    sellNiceTiming 구현

--- trading_system/auto_trading_system.h
+++ trading_system/auto_trading_system.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 
 #include "stock_brocker_driver.h"
 #include "timer.h"
@@ -21,7 +22,9 @@ class AutoTradingSystem {
     int getPrice(const std::string& stockCode) {}
 
     //증권사선택기능: selectStockBrocker( )
-    void selectStockBroker(StockBrokerDriverInterface* driver) {}
+    void selectStockBroker(StockBrokerDriverInterface* driver) {
+        this->driver = driver;
+    }
 
     //기능1 : buyNiceTiming(종목, 총 금액)
     //• 200ms 주기로 3회 가격을 읽고, 가격이올라가는추세인지파악한다.
@@ -33,12 +36,34 @@ class AutoTradingSystem {
     //• 200ms 주기로 3회 가격을 읽고, 가격이 내려가는추세인지파악한다.
     //• 가격이내려가는추세라면, 사용자가설정한수량만큼주식을모두매도한다.
     //• 마지막에읽은가격으로매도한다
-    void sellNiceTiming(const std::string& stockCode, int numberOfStock) {}
+    void sellNiceTiming(const std::string& stockCode, int numberOfStock) {
+        std::vector<int> prices = readPricesThreeTimes(stockCode);
+        if (isDecreasing(prices)) {
+            driver->sell(stockCode, prices.back(), numberOfStock);
+        }
+    }
 
     //Dependency Injection for timer
-    void setTimer(TimerInterface* timer) {}
+    void setTimer(TimerInterface* timer) {
+        this->timer = timer;
+    }
 
 private:
+    std::vector<int> readPricesThreeTimes(const std::string& stockCode) {
+        std::vector<int> prices;
+        prices.push_back(driver->getPrice(stockCode));
+        timer->sleep(READ_INTERVAL_MS);
+        prices.push_back(driver->getPrice(stockCode));
+        timer->sleep(READ_INTERVAL_MS);
+        prices.push_back(driver->getPrice(stockCode));
+        return prices;
+    }
+
+    bool isDecreasing(const std::vector<int>& prices) {
+        return prices[0] > prices[1] && prices[1] > prices[2];
+    }
+
+    static constexpr int READ_INTERVAL_MS = 200;
     StockBrokerDriverInterface* driver = nullptr;
     TimerInterface* timer = nullptr;
 };
