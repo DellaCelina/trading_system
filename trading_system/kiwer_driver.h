c144c05adcca8d91d34ce6f93638d5a597327db2
test <test@test.com>
2026-07-03 13:06:38 +0900

    [feature] kiwer driver 적용

---
+++ trading_system/kiwer_driver.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "stock_brocker_driver.h"
+#include "kiwer_api.h"
+
+struct KiwerDriver : public StockBrokerDriverInterface {
+    KiwerDriver(KiwerAPI* api) : api(api) {}
+
+    void login(const std::string& id, const std::string& pass) override {
+        api->login(id, pass);
+    }
+
+    void buy(const std::string& stockCode, int price, int count) override {
+        api->buy(stockCode, count, price);
+    }
+
+    void sell(const std::string& stockCode, int price, int count) override {
+        api->sell(stockCode, count, price);
+    }
+
+    int getPrice(const std::string& stockCode) override {
+        return api->currentPrice(stockCode);
+    }
+
+    KiwerAPI* api;
+};
