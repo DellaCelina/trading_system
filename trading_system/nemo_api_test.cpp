00d8c695f5b7ba70425e7f67aa8f2c7eea3af124
Deokhyun Ahn <dks0554@gmail.com>
2026-07-03 12:52:40 +0900

    [test] Add NEMO API unit test
    Signed-off-by: Deokhyun Ahn <dks0554@gmail.com>

---
+++ trading_system/nemo_api_test.cpp
@@ -0,0 +1,67 @@
+﻿#include "gmock/gmock.h"
+
+#include <functional>
+#include <numeric>
+#include <format>
+
+#include "nemo_api.h"
+
+using namespace testing;
+
+using std::string;
+using std::format;
+
+struct NemoAPITester : public Test {
+    string captureStdout(std::function<void()> f) {
+        internal::CaptureStdout();
+        f();
+        return internal::GetCapturedStdout();
+    }
+
+    NemoAPI api;
+};
+
+TEST_F(NemoAPITester, testCertificationOutput) {
+    const string ID = "TEST_ID";
+    const string pass = "TEST_PASS";
+    const auto golden = format("[NEMO]{} login GOOD\n", ID);
+
+    auto output = captureStdout([&] {
+        api.certification(ID, pass);
+        });
+    EXPECT_EQ(output, golden);
+}
+
+TEST_F(NemoAPITester, testPurchasingStock) {
+    const string stockCode = "STOCK_CODE";
+    const int price = 10;
+    const int count = 20;
+    const auto golden = format("[NEMO]{} buy stock ( price : {} ) * ( count : {})\n", stockCode, price, count);
+
+    auto output = captureStdout([&] {
+        api.purchasingStock(stockCode, price, count);
+        });
+    EXPECT_EQ(output, golden);
+}
+
+TEST_F(NemoAPITester, testSellingStock) {
+    const string stockCode = "STOCK_CODE";
+    const int price = 10;
+    const int count = 20;
+    const auto golden = format("[NEMO]{} sell stock ( price : {} ) * ( count : {})\n", stockCode, price, count);
+
+    auto output = captureStdout([&] {
+        api.sellingStock(stockCode, price, count);
+        });
+    EXPECT_EQ(output, golden);
+}
+
+TEST_F(NemoAPITester, testGetMarketPrice) {
+    const string stockCode = "STOCK_CODE";
+    const int minute = 10;
+
+    auto price = api.getMarketPrice(stockCode, minute);
+    EXPECT_THAT(price, Ge(5000));
+    EXPECT_THAT(price, Le(5900));
+}
+
