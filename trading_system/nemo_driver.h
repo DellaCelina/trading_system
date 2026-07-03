c39494b2a58b5a87a8479bed8e4e1b8cfca66457
Duckchan-Seo <duckchan.seo@samsung.com>
2026-07-03 13:32:40 +0900

    [feature] Remove empty lines for clean code

--- trading_system/nemo_driver.h
+++ trading_system/nemo_driver.h
@@ -7,24 +7,18 @@ struct NemoDriver : public StockBrokerDriverInterface {
     NemoDriver(NemoAPI* api) { nemo_api = api; }
 
     void login(const std::string& id, const std::string& pass) override {
-
         return nemo_api->certification(id, pass);
     }
 
     void buy(const std::string& stockCode, int price, int count) override {
-
         return nemo_api->purchasingStock(stockCode, price, count);
-
     }
 
     void sell(const std::string& stockCode, int price, int count) override {
-
         return nemo_api->sellingStock(stockCode, price, count);
-
     }
 
     int getPrice(const std::string& stockCode) override {
-
         return nemo_api->getMarketPrice(stockCode, 1);
     }
 
