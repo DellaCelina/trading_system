db6ec7deacaec52fbe5c419c7c0a32873da56a61
Deokhyun Ahn <dks0554@gmail.com>
2026-07-03 14:56:22 +0900

    [feat] Add pricesNeededCount interface to get prices
    Signed-off-by: Deokhyun Ahn <dks0554@gmail.com>

--- trading_system/timing_strategy.h
+++ trading_system/timing_strategy.h
@@ -3,6 +3,8 @@
 #include <vector>
 
 struct TimingStrategyInterface {
+    // 필요한 Price 갯수
+    virtual size_t pricesNeededCount() = 0;
     virtual bool shouldBuy(const std::vector<int> priceHistory) = 0;
     virtual bool shouldSell(const std::vector<int> priceHistory) = 0;
 };
